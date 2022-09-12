f4ce0f0135016b9969bf17638e0dbe58df05c8e7
Najmul Faiz <faiznajmul@gmail>
2022-09-12 21:59:36 +0700

    Fixing Response

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const BpjsService = require('./src/BpjsService');
+import WSBpjs from "./src/Antrean/WSBpjs";
+
+const antrean = new WSBpjs();
+
+antrean.refPoli().then(function(response) {
+  console.log(response);
+});
--- src/BpjsService.js
+++ src/BpjsService.js
@@ -1,22 +1,31 @@
-const CryptoJS = require('crypto-js');
-const axios = require('axios');
-const LZString = require('lz-string');
+import * as dotenv from 'dotenv';
+import CryptoJS from 'crypto-js';
+import axios from 'axios';
+import LZString from 'lz-string';
+import _ from 'lodash';
 
-class BpjsService {
+dotenv.config();
+
+export default class BpjsService {
+  CONS_ID          = process.env.CONS_ID;
+  CONS_SECRET      = process.env.CONS_SECRET;
+  USERKEY_VCLAIM   = process.env.USERKEY_VCLAIM;
+  USERKEY_ANTREAN  = process.env.USERKEY_ANTREAN;
+  BASE_URL_VCLAIM  = process.env.BASE_URL_VCLAIM;
+  BASE_URL_ANTREAN = process.env.BASE_URL_ANTREAN;
+
   constructor() {
-    this.BASE_URL = process.env.BASE_URL;
-    this.CONS_ID = process.env.CONS_ID;
-    this.CONS_SECRET = process.env.CONS_SECRET;
-    this.USERKEY = process.env.USERKEY;
+    this.setTimestamp();
+    this.setSignature();
   }
 
-  setHeader() {
+  setHeader(service_type) {
     this.headers = {
       'Content-Type': 'application/json',
-    'X-cons-id': this.BASE_URL,
-    'X-timestamp': this.timestamp,
-    'X-signature': this.signature,
-    'user_key': this.USERKEY,
+      'X-cons-id': this.CONS_ID,
+      'X-timestamp': this.timestamp,
+      'X-signature': this.signature,
+      'user_key': service_type == 'antrean' ? this.USERKEY_ANTREAN : this.USERKEY_VCLAIM,
     }
   }
 
@@ -29,41 +38,72 @@ class BpjsService {
     this.signature = CryptoJS.enc.Base64.stringify(hash);
   }
 
-  get() {
-    axios({
-      method: 'get',
-      url: this.BASE_URL + '/antreanrs/ref/poli',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-cons-id': this.CONS_ID,
-        'X-timestamp': timestamp,
-        'X-signature': signature,
-        'user_key': this.USERKEY,
-      }
-    }).then(function(res) {
-      let passphrase = this.CONS_ID + this.CONS_SECRET + timestamp;
-      let response = decrypt_data(passphrase, res.data.response);
-      console.log(response);
+  requestAntrol(method, service, data = []) {
+    const self = this;
+    this.setHeader('antrean');
+
+    return axios({
+      method: method,
+      url: this.BASE_URL_ANTREAN + service,
+      headers: this.headers,
+      data: method == 'post' ? data : null,
+    }).then(function (response) {
+      return self.responseAntrol(response.data);
     })
       .catch(function (error) {
         console.log(error);
       });
   }
 
-  responseAntrol(passphrase, data) {
-    let key = CryptoJS.enc.Hex.parse(CryptoJS.SHA256(passphrase).toString());
-    let iv = CryptoJS.enc.Hex.parse(CryptoJS.SHA256(passphrase).toString().substr(0, 32));
-    let decrypted = CryptoJS.AES.decrypt(data, key, {
+  responseAntrol(data) {
+    const result = data;
+
+    if(_.includes([200, 1], result.metadata.code) && result.response && _.isString(result.response)) {
+      return this.decryptResponse(result.metadata, result.response);
+    }
+
+    return data;
+  }
+
+  requestVClaim(method, service) {
+    const self = this;
+    this.setHeader('vclaim');
+
+    return axios({
+      method: method,
+      url: this.BASE_URL_VCLAIM + service,
+      headers: this.headers
+    }).then(function (response) {
+      return self.responseVClaim(response.data);
+    })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  responseVClaim(data) {
+    const result = data;
+
+    if(result.metaData.code == 200 && _.isString(result.response)) {
+      return this.decryptResponse(result.metaData, result.response);
+    }
+
+    return data;
+  }
+
+  decryptResponse(metadata, response) {
+    const passphrase = this.CONS_ID + this.CONS_SECRET + this.timestamp;
+    const key = CryptoJS.enc.Hex.parse(CryptoJS.SHA256(passphrase).toString());
+    const iv = CryptoJS.enc.Hex.parse(CryptoJS.SHA256(passphrase).toString().slice(0, 32));
+    const decrypted = CryptoJS.AES.decrypt(response, key, {
       iv: iv,
       mode: CryptoJS.mode.CBC,
       padding: CryptoJS.pad.Pkcs7
-    });
-  
-    decrypted = decrypted.toString(CryptoJS.enc.Utf8);
-    decompressed = LZString.decompressFromEncodedURIComponent(decrypted);
-    
-    return decompressed;
+    }).toString(CryptoJS.enc.Utf8);
+
+    return {
+      "metadata": metadata,
+      "response": JSON.parse(LZString.decompressFromEncodedURIComponent(decrypted))
+    };
   }
 }
-
-export default BpjsService
